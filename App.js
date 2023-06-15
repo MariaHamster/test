a06a8478684427531e0430e34fb4495717e3997c
Maria Nikulshina <nikulshina-ms@yandex.ru>
2023-06-15 15:06:44 +0300

    test

---
+++ App.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const os = require("os");
+const userInfo = os.userInfo();
+const uid = userInfo.uid;
+const hostname = '0.0.0.0';
+const port = 1000 + uid;
+
+// const hostname = '0.0.0.0';
+// const port = 5050;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World !');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
